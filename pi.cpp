ebb18a00a3679a67b39b8dae73cb54dcad1e4263
Po-Nan Li <lishantreasure@gmail.com>
2015-02-02 11:27:31 +0800

    N can be passed through arg

--- pi.cpp
+++ pi.cpp
@@ -1,6 +1,6 @@
 // ping_pong.cpp
 
-#define N 100
+
 
 #include <iostream>
 #include <ctime>
@@ -14,6 +14,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {	
+
+	N = 1000;
+	if (argc > 1)
+		N = atoi(argv[1]);
+	
 	srand(time(NULL));
 	MPI_Init(&argc, &argv);
 
